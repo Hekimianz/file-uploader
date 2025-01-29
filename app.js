daf671abe0aafbe982a74ab1b256857413d27454
Aram Hekimian <guerrerohek121@gmail.com>
2025-01-29 09:43:30 -0600

    set up passport

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const initializePassport = require("./config/passport");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,7 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+initializePassport(passport);
 app.use("/sign-up", (req, res) =>
   req.user ? res.render("home") : res.render("signup")
 );
---
+++ config/passport.js
@@ -0,0 +1,48 @@
+const { PrismaClient } = require("@prisma/client");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+const prisma = new PrismaClient();
+
+function initialize(passport) {
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = prisma.user.findUnique({
+          where: { username: username },
+        });
+        if (!user) {
+          return done(null, false, {
+            message: "No user with that email has been found",
+          });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+}
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: id },
+    });
+    if (!user) {
+      return done(null, false);
+    }
+    return done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = initialize;
